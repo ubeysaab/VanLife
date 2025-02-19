c70382d4fe4909ba752819066c660751e8e9bed6
ubeysaab <aboosh.sa262000@gmail.com>
2025-02-19 23:17:51 +0300

    :heavy_plus_sign: addin YUP for form Validation and apply validation to login form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFormik } from 'formik'
+import validationSchema from "../Schemas/shemas"
 function Login() {
 
 
@@ -9,17 +10,32 @@ function Login() {
       initialValues :{
         "email":"",
         "password":""
-      }
+      },
+      // after adding validation schema here our form is gonna know to use this schema and when we try to submit  the form it's will validate it and display any error if any of the fields didn't pass.
+      validationSchema:validationSchema,
+
+
+
+      //formik.handleSubmit is gonna call the code below
+       onSubmit: ()=>{
+        console.log("submitted")
+       }
     }
   )
-  console.log(formik)
+  
   return (
-    <div>
-      <form autoComplete='off'>
-        <label for='email'>Email</label>
+    <main className='form-container'>
+      <div>
+      <h2 className='form-header'>Sign in to your account</h2>
+      <form 
+
+      onSubmit={formik.handleSubmit}
+      autoComplete='off'>
+        {/* <label for='email'>Email</label> */}
         <input type="email" 
         name="email" 
         id="email"
+        className='form-input'
         value={formik.values.email}
         // in onChange instead of defining our function we gonna use formik.handleChange which come from formik and also know to take the one change event and set the formik state based on what ever value it's looking for 
         onChange={formik.handleChange}
@@ -28,8 +44,10 @@ function Login() {
 
         onBlur={formik.handleBlur}
         /> 
-        <label for='password'> Password</label>
+        {formik.errors &&formik.touched.email && <span className='error-message'>{formik.errors.email}</span>}
+        {/* <label for='password'> Password</label> */}
         <input type="password" 
+        className='form-input'
         name="password" 
         id="password"
         value={formik.values.password}
@@ -37,13 +55,19 @@ function Login() {
         placeholder='Enter Your Password'
         onBlur={formik.handleBlur}
         /> 
+          {formik.errors&&formik.touched.password && <span className='error-message'>{formik.errors.password}</span>}
         
 
 
-        <button type="submit">Submit</button>
+        <button type="submit" className='btn--simple form-btn'>Submit</button>
       </form>
-    </div>
+      </div>
+    </main>
   )
 }
 
 export default Login
+
+// formik is just control form states it doesn't validate any thing 
+
+// so we need to use YUP which is validaiton library which is really works well with formik it's actually built in to formik to use that library in order to use it we need to create a Schema for our form and our schema basically is gonna define these {initial values properties } and it's gonna basically say what those properties types should be 
---
+++ src/Schemas/shemas.js
@@ -0,0 +1,22 @@
+// to create a schema we first neet to import YUP
+
+
+
+import * as yup from 'yup';
+
+
+const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+
+const basicShema = yup.object(
+//  this schema is gonna be equal to an object that we defined with shape
+// inside we put the different values that we want to validate for 
+  {
+    email:yup.string().email("Please Enter a valid E-mail").required(),
+    password:yup.string().min(8).matches(passwordRules,{message : "please create a stronger password "}).required()
+
+
+    // confirmPassword : yp.string().oneof([yep.ref("password"),null],'Passwords must match')
+  }
+)
+
+export default basicShema
